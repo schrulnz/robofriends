b1629e0726c7f19c089a28377164922c75e0d0ca
steffenzimmermann <steffenzimmermann@bitbucket.org>
2020-04-17 14:19:18 +0200

    deplay gh-pages

---
+++ build/precache-manifest.21088c05eef1bbdfd40268ce88b71ecf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a309d34eee2c7ce4a6f501a6d1437db6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9a8db8c81b672b7aa925",
+    "url": "/static/css/main.140eaf0b.chunk.css"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/static/js/2.4b6b2329.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.4b6b2329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a8db8c81b672b7aa925",
+    "url": "/static/js/main.fbd557c8.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d4efaf5f5a15652d7654e64ad130e15.js"
+  "/precache-manifest.21088c05eef1bbdfd40268ce88b71ecf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
